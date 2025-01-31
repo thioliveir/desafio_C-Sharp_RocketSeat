6daf49ccb1f03b1b0022d5a222de332031eab0c2
Thiago Ferreira <thiago.oliveira.1446007@sga.pucminas.br>
2025-01-31 14:23:10 -0300

    Fourth Challenge by RocketSeat
    App number of characters

---
+++ 04. Numeros Caracteres/CaracterNumber/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+
+namespace CaracterNumber;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite uma ou mais palavras (separadas por espaços): ");
+        string input = Console.ReadLine();
+
+        // Iniciando um contador de caracteres
+        int contador = 0;
+
+        foreach (char c in input)
+        {
+            if(c != ' ')
+            {
+                contador++;
+            }
+        }
+
+        Console.WriteLine($"A quantidade de caracter na frase é: {contador}");
+        
+    }
+}
