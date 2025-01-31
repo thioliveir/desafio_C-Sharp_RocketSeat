75a5e6c4af393f63c69388d03a07d137bcfbf09e
Thiago Ferreira <thiago.oliveira.1446007@sga.pucminas.br>
2025-01-31 14:20:57 -0300

    Third Challenge by Rocketseat
    App mathematical operations

---
+++ 03. Operações Matemáticas/OperacoesMatematicas/Operacoes.cs
@@ -0,0 +1,46 @@
+﻿using System.ComponentModel.Design;
+using System.Diagnostics;
+
+namespace OperacoesMatematicas;
+
+public class Operacoes
+{
+    public double Somar(double number1, double number2)
+    {
+        return number1 + number2;
+    }
+    public double Subtrair(double number1, double number2)
+    {
+        if(number1 > number2)
+        {
+            return number1 - number2;
+        } else
+        {
+            return number2 - number1;
+        }
+    }
+    public double Multiplica(double number1, double number2)
+    {
+        return (number1 * number2);
+    }
+    public double Divide(double number1, double number2)
+    {
+        if(number1 > number2 && number1 != 0 && number2 != 0)
+        {
+            return number1 / number2;
+        } else if (number2 > number1 && number1 != 0 && number2 != 0)
+        {
+            return number2 / number1;
+        }
+
+        return 0;
+        
+    }
+    public double Media(double number1, double number2)
+    {
+        double media = (number1 + number2) / 2;
+
+        return media;
+
+    }
+}
---
+++ 03. Operações Matemáticas/OperacoesMatematicas/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace OperacoesMatematicas;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite o primeiro número: ");
+        double number1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Digite outro numero: ");
+        double number2 = Convert.ToDouble(Console.ReadLine());
+
+        Operacoes calculo = new Operacoes();
+
+        Console.WriteLine("Soma: " + calculo.Somar(number1, number2));
+        Console.WriteLine("Subtração: " + calculo.Subtrair(number1, number2));
+        Console.WriteLine("Multiplicação: " + calculo.Multiplica(number1, number2));
+        Console.WriteLine("Divisão: " + calculo.Divide(number1, number2));
+        Console.WriteLine("A média entre eles é: " + calculo.Media(number1, number2));
+
+
+    }
+}
