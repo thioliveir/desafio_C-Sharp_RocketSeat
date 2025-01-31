ada1d20d6e0b9d58e04c398b149714162db9cdbe
Thiago Ferreira <thiago.oliveira.1446007@sga.pucminas.br>
2025-01-31 14:19:22 -0300

    Second Challenge by RocketSeat
    App concatenating first name and last name

---
+++ 02. Concatenando Nome e Sobrenome/NameLastname/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace NameLastname;
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Digite o seu nome: ");
+        string name = Console.ReadLine();
+
+        Console.Write("Digite o seu sobrenome: ");
+        string lastname = Console.ReadLine();
+
+        Console.WriteLine("O seu nome completo é: " + name + " " + lastname);
+    }
+}
