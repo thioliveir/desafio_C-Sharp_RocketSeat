8103f97453d7e8fbc107db7402be05829c4fb21f
Thiago Ferreira <thiago.oliveira.1446007@sga.pucminas.br>
2025-01-31 14:17:48 -0300

    Add first challenge by Rockeseat using C#
    App Welcome

---
+++ 01. Boas Vindas Personalizado/Welcome/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Welcome;
+
+class Program
+{
+    static void Main()
+    {
+        // Solicita ao usuário que digita seu nome
+        Console.Write("Digite seu nome: ");
+
+        // Lê o nome digitado e armazena na variável
+        string nome = Console.ReadLine();
+
+        // Exige a tela de boas-vindas com o nome do usuário
+        Console.WriteLine("Olá, " + nome + "! Seja muito bem-vindo!");
+    }
+}
